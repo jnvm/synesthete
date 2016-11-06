7e8937671c76e7f970cbedb27892ccff501b27fe
jnvm <j@nvm.io>
2016-11-06 02:30:36 +0000

    remove env var & add cli flags

--- cli.js
+++ cli.js
@@ -1,69 +1,101 @@
 #! /usr/bin/env node
 const crayon=require("crayon-terminal")
 	,crypto=require("crypto")
-
-if (!String.prototype.padStart) {
-	String.prototype.padStart = function (max, fillString) {
-		return padStart(this, max, fillString);
-	};
-}
-
-function padStart (text, max, mask) {
-	const cur = text.length;
-	if (max <= cur) {
-		return text;
+	,tinyc=require('tinycolor2')
+	,opts={
+		 match :{val:'\\S+',help:`assign the global, case-sensitive regex to match text to colorize
+${crayon.underline("Examples")}:
+-m . will match each character
+-m \\d+ will match all contiguous digits
+-m [^aeiouyAEIOUY]+ will match all contiguous non-vowels
+-m="\\s*\\S+" will match all the spaces before a nonspace, then adjacent nonspaces, etc
+Regex with spaces need to be strung: -m=\"[^a-z] {4}\\d+\\s*\"`}
+		,salt  :{val:'8D'    ,help:`assign anything here to generate new deterministic colors
+${crayon.underline("Examples")}:
+-s 9
+-s abcd
+-s $RANDOM for something different each time if you use bash`}
+		,invert:{val:false ,help:"flag colorizes background and makes text black. -w makes text white."}
+		,white :{val:false ,help:"flag makes background white"}
+		,black :{val:false ,help:"flag makes background black"}
+		,help  :{val:false ,help:"flag shows this help"}
 	}
-	const masked = max - cur;
-	let filler = String(mask) || ' ';
-	while (filler.length < masked) {
-		filler += filler;
-	}
-	const fillerSlice = filler.slice(0, masked);
-	return fillerSlice + text;
-}
-
-var data=''
-	,cachedWords={}
-
-process
-	.stdin
-	.resume()
-	.setEncoding('utf8')
-	.on('data', chunk=>data += chunk)
-	.on('end',()=>
-		process.stdout.write(
-			crayon.stripColor(data).replace(/(\S+)/g,(all,word)=>{
+	,args=require('minimist')(process.argv.slice(2),{
+		default:Object.keys(opts).reduce((set,part)=>{
+			set[part]=opts[part].val
+			return set
+		},{})
+		,alias:Object.keys(opts).reduce((set,part)=>{
+			set[part[0]]=part
+			return set
+		},{})
+		,unknown(x){
+			crayon.red.bold.log(`unknown arg: ${x} check --help for options`)
+			process.exit(1)
+		}
+	})
+	,colorize=(data)=>{
+		var cachedWords={}
+		return crayon.stripColor(data).replace(new RegExp(`(${args.match})`,"g"),(all,word)=>{
 				if(!cachedWords[word]){
-					var rgb=crypto.createHash('md5')
-							.update(word+(process.env.SYNESTHETE||''))
-							.digest()
-							.toString('hex')
+					var hex=crypto.createHash('md5')
+							.update(word+args.salt)
+							.digest('hex')
 							.slice(0, 6)
-							.split("")
-							.reduce((set,part,i)=>{
-								set.c+=part
-								if((i%2)){
-									set.push(parseInt(set.c,16))
-									set.c=''
-								}
-								return set
-							},Object.assign([],{c:''}))
-						//luminance check
-						,[r,g,b]=rgb
-							.map(c=>c/255)
-							.map(c=>
-								c<=0.03928
-								? c/12.92
-								: Math.pow((c+0.055)/1.055, 2.4)
-							)
-						,L=0.2126 * r + 0.7152 * g + 0.0722 * b					
-						,isLight=L > 0.15 //.179?
-						
-					if(!isLight) rgb=rgb.map(x=>255-x)
-					var back2hex=rgb.map(x=>x.toString(16).padStart(2,"0")).join("")
-					cachedWords[word]=crayon.foreground("#"+back2hex)(word)
+						,colo=tinyc(hex).saturate(20)
+						,mono=tinyc(args.white?"fff":"000")
+						,dir=args.white?"darken":"lighten"
+					
+					while(!tinyc.isReadable(colo,mono))
+						colo[dir](30)
+					
+					colo=colo.toHex()
+					mono=mono.toHex()
+					
+					var style=crayon.foreground(colo)
+					     if(args.invert) style=style.background(colo ).foreground(mono)
+					else if(args.black ) style=style.background("000").foreground(colo)
+					else if(args.white ) style=style.background("fff").foreground(colo)
+
+					cachedWords[word]=style(word)
 				}
 				return cachedWords[word]
 			})
-		)
-	)
+	}
+
+var data=''
+
+if(process.stdin.isTTY || args.help){
+	var flags=Object.keys(opts)
+		,longest=Math.max(...flags.map(x=>x.length))
+		,json=JSON.parse(require("fs").readFileSync("package.json"))
+	console.log([
+		 `\n${crayon.underline("DESCRIPTION")}: ${colorize(json.description)}`
+		,`${crayon.underline("USAGE")}: ${colorize('pipe text to it, like this:')}`
+		,`yourText | ${json.name} [-${flags.map(x=>x[0]).join("")}, all optional flags]`
+		,crayon.underline("FLAGS")+":"
+		,...flags.map(flag=>{
+			var defaultVal=opts[flag].val?`\n(default: ${opts[flag].val} )`:''
+				,aFlag=flag
+				,isBool=opts[flag].val.constructor==(true).constructor
+			while(aFlag.length<longest) aFlag+=" "
+			var left=`-${flag[0]}, --${aFlag} :`
+				,txt=crayon.foreground("#ccc")(" "+(opts[flag].help+defaultVal).replace(/\n/g,"\n"+left.replace(/./g,' ')))
+			
+			if(isBool){
+				args[flag]=!args[flag]
+				txt=colorize(txt)
+				args[flag]=!args[flag]
+			}
+			return crayon.bold(left)+txt
+		})
+		,""
+	].join("\n\n"))
+	process.exit(0)
+}
+else process
+	.stdin
+	.resume()
+	.setEncoding('utf8')
+	.on('data',chunk=>data+=chunk)
+	.on('end',()=>process.stdout.write(colorize(data)))
--- readme.md
+++ readme.md
@@ -1,43 +1,82 @@
 # Synesthete
 ![Deterministically colorize cli words.](https://cldup.com/ZExW4HGpTe.PNG)
 
-
-Words are considered nonspace clumps.
-
-Salt color mapping by setting env var `SYNESTHETE`.
-
-A luminance check is performed to keep colors mostly bright enough to be readable on a dark background.
-
 This was originally made to better visually differentiate columns of same-length words in bash outputs / text-only interfaces.
 
-Plus the cli just needs more color sometimes.
+To be exact, hue is deterministic based on the matched text, lightness is varied to ensure readability.
 
-## Install
+Plus the command line just needs more color sometimes.
+
+## Install & Usage
 `npm i -g synesthete`
 
-## Example
 Regular usage:
 
 `> echo "You pass butter." | synesthete`
 
 ![example of cli text colored](https://cldup.com/sJzfLzGmRo.PNG)
 
-But that's a lot to type, let's alias it to sy:
+But that's a lot to type, let's alias it to just `sy`:
 
-`> alias sy=synesthete`
+`> alias sy='synesthete'`
 
-Setting salt:
+Ah, much better.
 
-`> export SYNESTHETE=1234 # but it could be anything, try a few`
+## Flags
+in `--help`:
+```
+-m, --match  : assign the global, case-sensitive regex to match text to colorize
+              Examples:
+              -m . will match each character
+              -m \d+ will match all contiguous digits
+              -m [^aeiouyAEIOUY]+ will match all contiguous non-vowels
+              -m="\s*\S+" will match all the spaces before a nonspace, then adjacent nonspaces, etc
+              Regex with spaces need to be strung: -m="[^a-z] {4}\d+\s*"
+              (default: \S+ )
 
-Then color mapping is different:
+-s, --salt   : assign anything here to generate new deterministic colors
+              Examples:
+              -s 9
+              -s abcd
+              -s $RANDOM for something different each time if you use bash
+              (default: 8D )
 
-![different colors](https://cldup.com/FaFDskJeOZ.PNG)
+-i, --invert : flag colorizes background and makes text black. -w makes text white.
 
-Should work on any text:
+-w, --white  : flag makes background white
 
-![ls -larth](https://cldup.com/S0NEu47Up8.PNG)
+-b, --black  : flag makes background black
 
-![tree -AL 2](https://cldup.com/7PDo3ystG4.PNG)
+-h, --help   : flag shows this help
+```
+## Examples
 
-: D
+!['list'](https://cldup.com/JN4EYN1ERe.PNG)
+
+Should work on weird characters, too:
+
+!['tree'](https://cldup.com/zOTYjgUhc3.PNG)
+
+Maybe you want to color the background and spaces surrounding words:
+
+!['list inverted'](https://cldup.com/bHWq2OrSH7.PNG)
+
+Or color the background with white text:
+
+!['list inverted white'](https://cldup.com/inrpbouCVm.PNG)
+
+Or color only digits on a black background:
+
+!['list black match numbers'](https://cldup.com/IAdx2Q8Dqp.PNG)
+
+Or color every character something different each time!
+
+!['randomly color each character'](https://cldup.com/PysJ7LB8BO.PNG)
+
+ If you find a combination you like, add it to your alias.
+
+Related info:
+* https://developer.mozilla.org/docs/Web/JavaScript/Guide/Regular_Expressions
+* https://regex101.com/
+* [VS Ramachandran on synesthesia](https://youtu.be/Rl2LwnaUA-k?t=17m53s)
+* [But...why?](https://www.youtube.com/watch?v=mG5v2udwOPY)
